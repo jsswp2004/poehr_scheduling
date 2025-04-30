1ca1ee379d90a9730c85c5a4498950915cffe724
jsswp2004 <jsswp2004@outlook.com>
2025-04-29 22:06:22 -0400

    added calendar view functionalities for edit and update and delete, also showed the name on the navbar to greet the user

--- users/serializers.py
+++ users/serializers.py
@@ -4,13 +4,15 @@ from .models import CustomUser
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
-        fields = ('id', 'username', 'email', 'password')
+        fields = ('id', 'username', 'email', 'password', 'first_name', 'last_name')
         extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
         user = CustomUser.objects.create_user(
             username=validated_data['username'],
             email=validated_data['email'],
-            password=validated_data['password']
+            password=validated_data['password'],
+            first_name=validated_data.get('first_name', ''),
+            last_name=validated_data.get('last_name', '')
         )
         return user
--- users/views.py
+++ users/views.py
@@ -1,7 +1,13 @@
 from rest_framework import generics
 from .serializers import UserSerializer
 from .models import CustomUser
+from rest_framework_simplejwt.views import TokenObtainPairView
+from .token_serializers import CustomTokenObtainPairSerializer
 
 class RegisterView(generics.CreateAPIView):
     queryset = CustomUser.objects.all()
     serializer_class = UserSerializer
+
+# ✅ This must be outside the RegisterView class
+class CustomTokenObtainPairView(TokenObtainPairView):
+    serializer_class = CustomTokenObtainPairSerializer
