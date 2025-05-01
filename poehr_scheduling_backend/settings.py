f44485459693e1ed4ac01e7f60c9202e596f0e22
jsswp2004 <jsswp2004@outlook.com>
2025-04-30 20:26:55 -0400

    transferred database to postgreSQL

--- poehr_scheduling_backend/settings.py
+++ poehr_scheduling_backend/settings.py
@@ -85,8 +85,14 @@ WSGI_APPLICATION = 'poehr_scheduling_backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'poehr_db',
+        'USER': 'jsswp2004',
+        'PASSWORD': 'krat25Miko!',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
