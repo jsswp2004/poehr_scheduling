d28dace5d55ef1aeceb88d2dfeef95c9f13bc5b1
jsswp2004 <jsswp2004@outlook.com>
2025-06-05 19:01:02 -0400

    refinements

--- frontend/src/pages/OverviewPage.js
+++ frontend/src/pages/OverviewPage.js
@@ -1,5 +1,6 @@
 import { Dialog, DialogTitle, DialogContent, DialogActions, Button, Typography } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
+import OverviewImage from '../assets/dashboard_overview.png'; // Placeholder for the overview image
 
 export const OverviewPage = () => {
   const navigate = useNavigate();
@@ -15,11 +16,23 @@ export const OverviewPage = () => {
         <div style={{ textAlign: 'center', marginBottom: '1rem' }}>
           {/* Image placeholder */}
           <div style={{ width: '100%', height: '200px', background: '#ccc', display: 'flex', alignItems: 'center', justifyContent: 'center', borderRadius: '8px' }}>
-            <Typography variant="subtitle1">Image Placeholder</Typography>
+            <Typography variant="subtitle1">        
+              <img 
+                src={OverviewImage} 
+                alt="Features Overview" 
+                className="features-image"
+                style={{
+                      width: '100%',
+                      height: '100%',
+                      objectFit: 'contain',
+                      borderRadius: '0px',
+                      padding: '0px'
+                    }} />
+            </Typography>
           </div>
         </div>
         <Typography variant="body1" paragraph>
-          POWER IT is a healthcare scheduling system built with a Django backend and React frontend. Clinics can upload events, holidays, staff lists and provider lists directly from the app, manage availability and block times, and send automated text and email reminders. Notifications keep both organization and system administrators informed whenever patients register or appointments are created, ensuring seamless collaboration across the team.
+          POWER Scheduler is a healthcare scheduling system built with a Django backend and React frontend. Clinics can upload events, holidays, staff lists and provider lists directly from the app, manage availability and block times, and send automated text and email reminders. Notifications keep both organization and system administrators informed whenever patients register or appointments are created, ensuring seamless collaboration across the team.
         </Typography>
       </DialogContent>
       <DialogActions>
