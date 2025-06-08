cfde8d15ffa23718a93325579c90ba193ac60f35
jsswp2004 <jsswp2004@outlook.com>
2025-06-08 14:37:22 -0400

    added email and sms to contact page

--- users/urls.py
+++ users/urls.py
@@ -12,8 +12,9 @@ from .views import (
     change_password,
     search_users,
     send_sms,
-    send_sms_email,
-    send_patient_email,
+    send_sms_email,    send_patient_email,
+    send_contact_email,  # Add the new contact email function
+    send_contact_sms,  # Add the new contact SMS function
     PatientDeleteView,
     OrganizationViewSet,  # ✅
     DownloadProvidersCSVTemplate,
@@ -39,8 +40,9 @@ urlpatterns = [
     path('search/', search_users, name='user-search'),
     path('', include('django_rest_passwordreset.urls', namespace='password_reset')),
     path('send-sms/', send_sms, name='send-sms'),
-    path('send-sms-email/', send_sms_email, name='send-sms-email'),
-    path('send-email/', send_patient_email, name='send-email'),
+    path('send-sms-email/', send_sms_email, name='send-sms-email'),    path('send-email/', send_patient_email, name='send-email'),
+    path('contact-email/', send_contact_email, name='contact-email'),  # New public contact endpoint
+    path('contact-sms/', send_contact_sms, name='contact-sms'),  # New public SMS endpoint
     path('patients/<int:user_id>/', PatientDeleteView.as_view(), name='patient-delete'),
     path('providers/download-template/', DownloadProvidersCSVTemplate.as_view(), name='providers-download-template'),
     path('providers/upload-csv/', UploadProvidersCSV.as_view(), name='providers-upload-csv'),
