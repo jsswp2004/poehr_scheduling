e4f3a6bfb7f7e03aafc4792a7743494b83b00fc2
jsswp2004 <31414054+jsswp2004@users.noreply.github.com>
2025-08-10 21:19:29 -0400

    � DEPLOYMENT TEST: Add obvious visual indicator - wrench emoji
    - This is a highly visible test to confirm GitHub Actions deployment is working
    - If you can see the wrench emoji (�) in the Patient Details page header, deployment is working
    - If not, there's still a deployment issue to investigate

--- frontend/src/pages/PatientDetailPage.js
+++ frontend/src/pages/PatientDetailPage.js
@@ -901,7 +901,7 @@ function PatientDetailPage() {
           mb: 2,
         }}
       >
-        <Typography variant="h5">Patient Details Check</Typography>
+        <Typography variant="h5">🔧 DEPLOYMENT TEST - Patient Details Check</Typography>
         <BackButton to="/patients" />
       </Box>
       {/* Show profile picture if available */}
